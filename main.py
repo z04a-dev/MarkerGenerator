3afe92b70662cb4fdb13e98065975f3c1b7a5465
alexander nikishin <ataka55555.88@gmail.com>
2023-01-25 00:23:40 +0300

    - added file reading (reads from library.txt) - program now saves final images in result folder - deprecated room argument, now using library only

--- main.py
+++ main.py
@@ -6,46 +6,52 @@ import qrcode, hashlib
 import argparse
 parser = argparse.ArgumentParser(description='mark generator')
 parser.add_argument('library', type=str, help='Library name')
-parser.add_argument('room', type=str, help='Room name')
 args = parser.parse_args()
 
-#Сборка строки для генерации qr кода
 DEFAULT_IDENTIFIER = "3257b0ae1f8d05fed50a757017a93688" #md5 идентификатор приложения
 
-LIBRARY_HASH = hashlib.md5(args.library.encode('utf-8')).hexdigest() #md5 идентификатор библиотеки
+def create(room):
+    #Сборка строки для генерации qr кода
 
-ROOM_HASH = hashlib.md5(args.room.encode('utf-8')).hexdigest() #md5 идентификатор кабинета
+    LIBRARY_HASH = hashlib.md5(args.library.encode('utf-8')).hexdigest() #md5 идентификатор библиотеки
 
-FINAL_HASH = DEFAULT_IDENTIFIER + " " + LIBRARY_HASH + " " + ROOM_HASH #сборка в одну строку
-#---------------
+    ROOM_HASH = hashlib.md5(room.encode('utf-8')).hexdigest() #md5 идентификатор кабинета
 
-width = 2808 #конечная ширина метки
-height = 1984 #конечная высота метки
-upscaleqr_size = 1150 #разрешение, до которого будет масштабироваться qr код (1150x1150 к примеру)
+    FINAL_HASH = DEFAULT_IDENTIFIER + " " + LIBRARY_HASH + " " + ROOM_HASH #сборка в одну строку
+    #---------------
 
-#создание qr кода
-qrimg = qrcode.make(FINAL_HASH)
-upqr = qrimg.resize((upscaleqr_size,upscaleqr_size),resample=1)
+    width = 2808 #конечная ширина метки
+    height = 1984 #конечная высота метки
+    upscaleqr_size = 1150 #разрешение, до которого будет масштабироваться qr код (1150x1150 к примеру)
 
-#создание фона
-img = Image.new(mode = "RGB", size = (width,height), color = (255,255,255))
+    #создание qr кода
+    qrimg = qrcode.make(FINAL_HASH)
+    upqr = qrimg.resize((upscaleqr_size,upscaleqr_size),resample=1)
 
-#заполнение диагональными полосками
-x = 0
-for number in range(40):
-    img1 = ImageDraw.Draw(img)
-    img1.line([(x - width,0),(x,height)], fill="black", width = 12)
-    x += width/15
+    #создание фона
+    img = Image.new(mode = "RGB", size = (width,height), color = (255,255,255))
 
-#вставка qr кода в центр изображения
-newImg = img.copy()
-newImg.paste(upqr, (int(width/2 - upscaleqr_size/2), int(height/2 - upscaleqr_size/2)))
+    #заполнение диагональными полосками
+    x = 0
+    for number in range(40):
+        img1 = ImageDraw.Draw(img)
+        img1.line([(x - width,0),(x,height)], fill="black", width = 12)
+        x += width/15
 
-#вставка стрелки
-arrow = Image.open('src/arrow.png').convert('RGBA')
-newImg.paste(arrow,(width-320,int(982/2)),arrow.convert('RGBA'))
-newImg.paste(arrow,(160,int(982/2)),arrow.convert('RGBA'))
+    #вставка qr кода в центр изображения
+    newImg = img.copy()
+    newImg.paste(upqr, (int(width/2 - upscaleqr_size/2), int(height/2 - upscaleqr_size/2)))
 
-#сохранение
-imgName = args.library + "-" + args.room + ".png"
-newImg.save(imgName)
+    #вставка стрелки
+    arrow = Image.open('src/arrow.png').convert('RGBA')
+    newImg.paste(arrow,(width-320,int(982/2)),arrow.convert('RGBA'))
+    newImg.paste(arrow,(160,int(982/2)),arrow.convert('RGBA'))
+
+    #сохранение
+    imgName = "result/" + args.library + "-" + room + ".png"
+    newImg.save(imgName)
+    
+with open("library.txt") as file:
+    for line in file:
+        room = line.rstrip()
+        create(room)
